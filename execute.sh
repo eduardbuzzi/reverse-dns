1b4b2b9ef050b6e8f6247b8c7eb44814289c385c
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-08-26 13:20:29 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -7,12 +7,14 @@ if [ -z $DOMAIN ]
 then
 principal
 fi
+RANDOM=$(shuf -i 1-1000 -n1)
 IP=$(host $DOMAIN | cut -d ' ' -f4 | head -n1)
+echo "$IP" > /tmp/111
 if [ "$IP" = "found:" ]
 then
 principal
 fi
-WHOIS=$(whois $IP | grep "inetnum:" | cut -d ' ' -f6 | cut -d '/' -f1 | cut -d '.' -f1,2,3)
+WHOIS=$(cat /tmp/111 | cut -d '.' -f1,2,3)
 for ip in $(seq 1 254);
 do
 RAN=$(shuf -i 1-1000 -n1)
@@ -24,12 +26,13 @@ IP1=$(cat /tmp/DN$RAN | cut -d '.' -f4 | head -n$i | tail -n1)
 IP2=$(cat /tmp/DN$RAN | cut -d '.' -f3 | head -n$i | tail -n1)
 IP3=$(cat /tmp/DN$RAN | cut -d '.' -f2 | head -n$i | tail -n1)
 IP4=$(cat /tmp/DN$RAN | cut -d '.' -f1 | head -n$i | tail -n1)
-DNS=$(cat /tmp/DN$RAN | cut -d ' ' -f2 | cut -d '.' -f1,2,3 | head -n$i | tail -n1)
+DNS=$(cat /tmp/DN$RAN | cut -d ' ' -f2 | sed 's/.$//' | head -n$i | tail -n1)
 echo " || IP => $IP1.$IP2.$IP3.$IP4 || DNS => $DNS"
 done
 rm /tmp/DN$RAN
 sleep 0.15
 done
+rm /tmp/111
 principal
 }
 principal
